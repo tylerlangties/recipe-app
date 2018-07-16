bbd4339fb867fe114500d03fff9e396e4b551949
Tyler Langties <tylerlangties@gmail.com>
2018-07-15 22:03:11 -0400

    uploading recipe-app

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Form from './components/Form';
+import Recipes from './components/Recipes';
+
+const API_KEY = "0f86be5ae7bdf40e6595ce7feea2942d";
+
+
+class App extends Component {
+  state = {
+    recipes: []
+  }
+  getRecipe = async (e) => {
+    const recipeName = e.target.elements.recipeName.value;
+    console.log(recipeName)
+    e.preventDefault();
+    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`);
+
+    const data = await api_call.json();
+    this.setState({ recipes: data.recipes })
+    console.log(this.state.recipes)
+  }
+
+  componentDidMount = () => {
+    const json = localStorage.getItem("recipes");
+    const recipes = JSON.parse(json);
+    this.setState({ recipes: recipes })
+  }
+
+   componentDidUpdate = () => {
+     const recipes = JSON.stringify(this.state.recipes);
+     localStorage.setItem("recipes", recipes);
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Recipe Search</h1>
+        </header>
+        <Form getRecipe={this.getRecipe} />
+        <Recipes recipes={this.state.recipes}/>
+      </div>
+    );
+  }
+}
+
+export default App;
